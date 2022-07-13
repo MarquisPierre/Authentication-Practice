7bed078e4a08a60ef46536865ace4af050d45496
Marquis Pierre <43345914+MarquisPierre@users.noreply.github.com>
2022-07-12 23:57:42 -0400

    Update app.js

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ app.post("/login", (req,res) => {
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => {
+app.listen(3000, () => {
   console.log(`Example app listening on port ${port}!`))
 }
 
